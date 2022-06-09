c2227235725e4e46925beaf8d6a9d77b293ed1a2
yongheonn <tiger9966@naver.com>
2022-06-09 19:22:56 +0900

    feat(front-end): add checking id valid

--- front-end/pages/register/index.js
+++ front-end/pages/register/index.js
@@ -1,12 +1,29 @@
 //fetch
-const url_id = "http://192.168.0.19/forms/register/id_check";
-
+const url_id = server_url + "/register/id_check";
+idMsg = document.getElementById("idMsg");
 document
-  .getElementById("user_id_check")
-  .addEventListener("click", checkDuplicateId);
+  .getElementById("user_id")
+  .addEventListener("change", checkIdValid);
+
+function checkIdValid() {
+  if(isInValidLenId())
+    return
+  checkDuplicateId()
+}
+
+function isInValidLenId() {
+  id = document.getElementById("user_id").value;
+  if(id.length > 20 || id.length < 5) {
+    idMsg.innerText = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다."
+    return true;
+  }
+
+  idMsg.innerText = ""
+  return false;
+}
 
 function checkDuplicateId() {
-  id = document.getElementsByName("user_id")[0].value;
+  id = document.getElementById("user_id").value;
   const option_id = {
     method: "POST",
     body: JSON.stringify({ id }),
@@ -15,5 +32,9 @@ function checkDuplicateId() {
       "Content-Type": "application/json",
     },
   };
-  fetch(url_id, option_id).then((response) => alert(response));
+
+  fetch(url_id, option_id).then((response) => response.json() )
+  .then(isInValid => isInValid ? idMsg.innerText = "이미 사용중이거나 탈퇴한 아이디입니다." 
+  : idMsg.innerText = "사용 가능한 아이디입니다.");
+  
 }
---
+++ front-end/pages/server_url.js
@@ -0,0 +1,1 @@
+const server_url = "http://192.168.0.22/forms"
